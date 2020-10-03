9b6dd2edbe89541e676817327cafd155e1ff402c
Jainish2004 <69842037+Jainish2004@users.noreply.github.com>
2020-10-03 08:56:40 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,1 +1,13 @@
-void noop();
+//this is header file
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+this is print function
+  printf("hello world");
+ 
+  
+  //we don't need to return any value because we have written viod main there
+//void noop();
+}
